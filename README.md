dc45d6dd5cc645e3762927adccf9b1b801defe7f
vagrant <vagrant@vagrant.vm>
2017-05-27 22:19:24 +0000

    Initial init

---
+++ README.md
@@ -0,0 +1,29 @@
+# Python - Postgresql - Log Analyis
+
+## Requirements
+
+- Python 3 (Python 2 will work also, but the code is optimized for Python 3)
+- Psycopg2
+- Postgresql
+- Vagrant
+
+## How to run
+
+1. Clone the files to where the "news" database is loaded
+
+2. Create 2 views in the "news" database using the commands below. These views are used in the third query.
+
+```sql
+-- Create errors_in_days view to be used in bad_days function
+create view errors_in_days as
+select date(time), count (status) as errors from log
+where status = '404 NOT FOUND'
+group by date order by errors desc;
+
+-- Create views_in_days to be used in bad_days function
+create view views_in_days as
+select date(time), count (*) as views from log
+group by date order by views desc;
+```
+
+3. Run `python3 sql_queries.py`
---
+++ sql_queries.py
@@ -0,0 +1,74 @@
+import psycopg2
+from decimal import *
+
+# Joining 2 tables: aticles and log
+pop_3posts_sql = """
+select title, count(path) as views
+from articles, log where '/article/' || articles.slug = log.path
+group by title order by views desc limit 3;
+"""
+
+# Joining all 3 tables and then calculate the sum of views using Subquery
+pop_authors_sql = """
+select name, sum(views) as views
+from (select articles.title, authors.name, count(log.path) as views
+    from articles, authors, log
+    where '/article/' || articles.slug = log.path
+    and articles.author = authors.id
+    group by authors.name, articles.title) as all_posts
+group by name order by views desc;
+"""
+
+# Joining 2 views: views_in_days and errors_in_days
+bad_days_sql = """
+select views_in_days.date, views, errors
+from views_in_days join errors_in_days
+on views_in_days.date = errors_in_days.date
+order by errors desc;
+"""
+
+db_name = 'news'
+
+def pop_3posts_all():
+    """Return the most popular three articles of all time."""
+    db = psycopg2.connect(database=db_name)
+    c = db.cursor()
+    c.execute(pop_3posts_sql)
+    result = c.fetchall()
+    c.close()
+    db.close()
+    for a in result:
+        print (a[0], "        ", a[1], "views")
+
+print ("Three most popular articles of all time")
+print (pop_3posts_all())
+
+def pop_authors_all():
+    """Return the most popular article authors of all time."""
+    db = psycopg2.connect(database=db_name)
+    c = db.cursor()
+    c.execute(pop_authors_sql)
+    result = c.fetchall()
+    c.close()
+    db.close()
+    for a in result:
+        print (a[0], "        ", a[1], "views")
+
+print ("Most poppular authors of all time")
+print (pop_authors_all())
+
+def bad_days():
+    """Return days more than 1 percent of requests lead to errors."""
+    db = psycopg2.connect(database=db_name)
+    c = db.cursor()
+    c.execute(bad_days_sql)
+    result = c.fetchall()
+    c.close()
+    db.close()
+    for a in result:
+        if float(a[2])/float(a[1])*100 > 1:
+            print (a[0], "    ", float(a[2])/float(a[1])*100,"%")
+        else: pass
+
+print ("Which days more than 1% of requests lead to errors?")
+print (bad_days())
