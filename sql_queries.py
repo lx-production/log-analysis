2cfc60c3e8c51d5fc5d710ec9b3dfa4a1db64e6a
Huy Nguyen <willber.es@gmail.com>
2017-05-29 22:13:52 +0000

    Refactor sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -1,5 +1,6 @@
+#! /usr/bin/env python3
 import psycopg2
-from decimal import *
+# from decimal import *
 
 # Joining 2 tables: aticles and log
 pop_3posts_sql = """
@@ -29,46 +30,56 @@ order by errors desc;
 
 db_name = 'news'
 
+
+def connect():
+    try:
+        db = psycopg2.connect(dbname=db_name)
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Can't connect to database")
+
+
 def pop_3posts_all():
     """Return the most popular three articles of all time."""
-    db = psycopg2.connect(database=db_name)
-    c = db.cursor()
-    c.execute(pop_3posts_sql)
-    result = c.fetchall()
-    c.close()
+    db, cursor = connect()
+    cursor.execute(pop_3posts_sql)
+    result = cursor.fetchall()
+    cursor.close()
     db.close()
     for a in result:
-        print (a[0], "        ", a[1], "views")
+        print (a[0], '{:>10}{}'.format(a[1], " views"))
 
 print ("Three most popular articles of all time")
 print (pop_3posts_all())
 
+
 def pop_authors_all():
     """Return the most popular article authors of all time."""
-    db = psycopg2.connect(database=db_name)
-    c = db.cursor()
-    c.execute(pop_authors_sql)
-    result = c.fetchall()
-    c.close()
+    db, cursor = connect()
+    cursor.execute(pop_authors_sql)
+    result = cursor.fetchall()
+    cursor.close()
     db.close()
     for a in result:
-        print (a[0], "        ", a[1], "views")
+        print ('{:>25}{}{}{}'.format(a[0], "    ", a[1], " views"))
 
 print ("Most poppular authors of all time")
 print (pop_authors_all())
 
+
 def bad_days():
     """Return days more than 1 percent of requests lead to errors."""
-    db = psycopg2.connect(database=db_name)
-    c = db.cursor()
-    c.execute(bad_days_sql)
-    result = c.fetchall()
-    c.close()
+    db, cursor = connect()
+    cursor.execute(bad_days_sql)
+    result = cursor.fetchall()
+    cursor.close()
     db.close()
     for a in result:
         if float(a[2])/float(a[1])*100 > 1:
-            print (a[0], "    ", float(a[2])/float(a[1])*100,"%")
-        else: pass
+            print (a[0], "    ", float(a[2])/float(a[1])*100, "%")
+        else:
+            pass
 
 print ("Which days more than 1% of requests lead to errors?")
 print (bad_days())
